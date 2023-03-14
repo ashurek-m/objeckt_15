12221fb408a9ff5e760ccdcd871dc56a50f3be69
oshurek_m <thedarthset@gmail.com>
2023-03-14 13:13:37 +0300

    открываем браузер и входим в аккаунт диспетчера

--- cod/start_cod_test.py
+++ cod/start_cod_test.py
@@ -1,8 +1,27 @@
 from selenium import webdriver
+import time
 
-
-url_site_on = "https://www.google.by/"
+url_site_on = "https://zyfra:8001"
 
 browser = webdriver.Chrome(executable_path='src\\chromedriver.exe')
-browser.get(url_site_on)
 
+
+def enter():
+    try:
+        browser.get(url_site_on)
+        login_xpath = "/html/body/div/div/div/div/form/div[3]/div/div/div/input"
+        password_path = "/html/body/div/div/div/div/form/div[4]/div/div/div/span/input"
+        buttom = "/html/body/div/div/div/div/form/button"
+        time.sleep(10)
+        browser.find_element(by='xpath', value=login_xpath).send_keys('Ошурек М.П.')
+        browser.find_element(by='xpath', value=password_path).send_keys(1900)
+        browser.find_element(by='xpath', value=buttom).click()
+
+        time.sleep(5)
+
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        browser.close()
+        browser.quit()
