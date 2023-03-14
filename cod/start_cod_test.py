1bc95edb732e69b06dc77c971d2ed32a56d1524f
oshurek_m <thedarthset@gmail.com>
2023-03-14 13:28:44 +0300

    открываем браузер и входим в аккаунт диспетчера. Обходит ошибку не безопастной страницы

--- cod/start_cod_test.py
+++ cod/start_cod_test.py
@@ -9,10 +9,14 @@ browser = webdriver.Chrome(executable_path='src\\chromedriver.exe')
 def enter():
     try:
         browser.get(url_site_on)
+        buttom_dop = "/html/body/div/div[2]/button[3]"
+        browser.find_element(by='xpath', value=buttom_dop).click()
+        small_link = "/html/body/div/div[3]/p[2]/a"
+        browser.find_element(by='xpath', value=small_link).click()
         login_xpath = "/html/body/div/div/div/div/form/div[3]/div/div/div/input"
         password_path = "/html/body/div/div/div/div/form/div[4]/div/div/div/span/input"
         buttom = "/html/body/div/div/div/div/form/button"
-        time.sleep(10)
+        time.sleep(5)
         browser.find_element(by='xpath', value=login_xpath).send_keys('Ошурек М.П.')
         browser.find_element(by='xpath', value=password_path).send_keys(1900)
         browser.find_element(by='xpath', value=buttom).click()
@@ -25,3 +29,6 @@ def enter():
     finally:
         browser.close()
         browser.quit()
+
+
+enter()
