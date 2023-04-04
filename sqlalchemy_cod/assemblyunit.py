b270c90e90925969292d4ed68fa9f29f2b3aca01
oshurek_m <thedarthset@gmail.com>
2023-04-04 13:42:59 +0300

    содана класс таблицы с ДСЕ

--- sqlalchemy_cod/assemblyunit.py
+++ sqlalchemy_cod/assemblyunit.py
@@ -1,8 +1,20 @@
 from database import Base, SessionLocal
-from sqlalchemy import Column, Integer, String, func
+from sqlalchemy import Column, Integer, String, func, Text, DateTime
 
 
 class AssemblyUnit(Base):
     __tablename__ = 'AssemblyUnit'
     Id = Column(Integer, primary_key=True)
-
+    Code = Column(String)
+    Name = Column(String)
+    Description = Column(Text)
+    AssemblyUnitTypeId = Column(Integer)
+    AssemblyUnitGroupId = Column(Integer)
+    IsDeleted = Column(Integer)
+    IntegrationId = Column(Text)
+    AttachedFilePathList = Column(Text)
+    DefaultSpecificationId = Column(Integer)
+    RowAddDate = Column(DateTime)
+    RowAddUser = Column(Text)
+    RowUpdateDate = Column(DateTime)
+    RowUpdateUser = Column(Text)
