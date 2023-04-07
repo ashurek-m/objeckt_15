cbaab7def2e8c055304aec211ecf4d2d761ec0f9
zyfra_pc <maxim.oshurek@reciftech.com>
2023-04-07 16:56:41 +0300

    удалось подключение если выполнять код на томже компе где и база

--- sqlalchemy_cod/database.py
+++ sqlalchemy_cod/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = r"mssql+pymssql://directus:12345@ZYFRA\SQLEXPRESS:1433/Industry" #?driver=ODBC+Driver+17+for+SQL+Server"
+SQLALCHEMY_DATABASE_URL = r"mssql+pymssql://directus:12345@localhost:1433/Industry" #?driver=ODBC+Driver+17+for+SQL+Server"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
